8136ca2bd097388f7f250f5fcf666592e2fec1d6
Just-like-Phoenix <nikita.170385@gmail.com>
2023-05-08 02:27:31 +0300

    Work In Progres

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,32 +1,43 @@
-import React from "react";
+import Card from "react-bootstrap/esm/Card";
+import ToggleButton from "react-bootstrap/esm/ToggleButton";
 
 import "./Main.scss";
-import Card from "react-bootstrap/esm/Card";
-import ToggleButtonGroup from "react-bootstrap/esm/ToggleButtonGroup";
-import ToggleButton from "react-bootstrap/esm/ToggleButton";
+import { useState } from "react";
+import ButtonGroup from "react-bootstrap/esm/ButtonGroup";
 
 export interface IMainProps {}
 const MainPage: React.FunctionComponent<IMainProps> = (props) => {
+  const [radioValue, setRadioValue] = useState("1");
+
+  const radios = [
+    { name: "Active", value: "1" },
+    { name: "Radio", value: "2" },
+    { name: "Radio", value: "3" },
+  ];
+
   return (
     <div className="main">
       <Card className="filter">
-        <ToggleButtonGroup
-          className="filterButtons"
-          size="sm"
-          type="radio"
-          name="options"
-          defaultValue={1}
-        >
-          <ToggleButton id="tbg-radio-1" value={1}>
-            Radio 1
-          </ToggleButton>
-          <ToggleButton id="tbg-radio-2" value={2}>
-            Radio 2
-          </ToggleButton>
-          <ToggleButton id="tbg-radio-3" value={3}>
-            Radio 3
-          </ToggleButton>
-        </ToggleButtonGroup>
+        <ButtonGroup>
+          {radios.map((radio, idx) => (
+            <ToggleButton
+              style={{
+                backgroundColor: "#FFFFFF",
+                borderColor: "#D9D9D9",
+                color: "black",
+              }}
+              key={idx}
+              id={`radio-${idx}`}
+              type="radio"
+              name="radio"
+              value={radio.value}
+              checked={radioValue === radio.value}
+              onChange={(e) => setRadioValue(e.currentTarget.value)}
+            >
+              {radio.name}
+            </ToggleButton>
+          ))}
+        </ButtonGroup>
       </Card>
     </div>
   );
